56351040859f03c2444a20865a12bd42db8f63ea
Ariel522 <arik9211@mail.ru>
2024-07-10 19:31:57 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_va = 31, 'ira', 25, 'oleg', 'turtle'
+print(immutable_va) # кортеж нельзя изменть, потому что он опсывает неизменяемую последовательность
+mutable_list = ['banana', 'bool', 'onion']
+print (mutable_list)
+mutable_list .remove('bool')
+print (mutable_list)
+mutable_list .append('bread')
+print (mutable_list)
